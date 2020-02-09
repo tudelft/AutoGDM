7e30d38f737a0355f2efd502f8d61deae55dc228
Duisterhof <bduisterhof@student.tudelft.nl>
2020-02-09 12:28:44 +0100

    working initial CAD modding

--- runtime/gen_CAD.py
+++ runtime/gen_CAD.py
@@ -1,14 +1,17 @@
 """
 Generating .stl files for CFD computations
 """
-
 import sys
 sys.path.insert(1,'../settings')
 
+import argparse
 from settings import *
-from stl import mesh
-import math
 import numpy
+import cadquery as cq
+from cadquery import exporters
+from io import StringIO
+import shutil
+from PIL import Image
 
 class CAD:
     def __init__(self):
@@ -16,26 +19,25 @@ class CAD:
         self.size_y = size_y
         self.size_z = size_z
 
+    def parse_settings(self):
+        parser = argparse.ArgumentParser()
+
+        # add long and short argument
+        parser.add_argument("--input_file", help="input image")
+        parser.add_argument("--output_file", help="output file (.stl)")
+
+        # read arguments from the command line
+        self.args = parser.parse_args()
+
     def draw_outline(self):
-        data = numpy.zeros(6, dtype=mesh.Mesh.dtype)
-        # Top of the cube
-        data['vectors'][0] = numpy.array([[0, 1, 1],
-                                        [1, 0, 1],
-                                        [0, 0, 1]])
-        data['vectors'][1] = numpy.array([[1, 0, 1],
-                                        [0, 1, 1],
-                                        [1, 1, 1]])
-        # Front face
-        data['vectors'][2] = numpy.array([[1, 0, 0],
-                                        [1, 0, 1],
-                                        [1, 1, 0]])
-        data['vectors'][3] = numpy.array([[1, 1, 1],
-                                        [1, 0, 1],
-                                        [1, 1, 0]])
-        # Left face
-        data['vectors'][4] = numpy.array([[0, 0, 0],
-                                        [1, 0, 0],
-                                        [1, 0, 1]])
-        data['vectors'][5] = numpy.array([[0, 0, 0],
-                                        [0, 0, 1],
-                                        [1, 0, 1]])
+        self.r = cq.Workplane("front").rect(self.size_x,self.size_y)
+
+    def read_image(self):
+        s = '../env_pics/'+str(self.args.input_file)
+        self.image = Image.open(s)
+        self.image.show()
+
+if __name__ == "__main__":
+    CAD = CAD()
+    CAD.parse_settings()
+    CAD.read_image()
