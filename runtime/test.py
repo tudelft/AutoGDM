37c6d8108a09572ec4618f9850619159684994c8
Duisterhof <bduisterhof@student.tudelft.nl>
2020-02-09 16:32:52 +0100

    bug to be fixed: can't cut out many rectangles

--- runtime/test.py
+++ runtime/test.py
@@ -1,38 +1,15 @@
-import cadquery as cq
-from cadquery import exporters
-from io import StringIO
-import shutil
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt    
 
-# These can be modified rather than hardcoding values for each dimension.
-plate_radius = 2.0          # The radius of the plate that will be extruded
-hole_pattern_radius = 0.25  # Radius of circle where the holes will be placed
-thickness = 0.125           # The thickness of the plate that will be extruded
+img = cv2.imread('../env_pics/ts_1.png')
+gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+edges = cv2.Canny(gray,50,150,apertureSize = 3)
+minLineLength = 2
+maxLineGap = 100
+lines = cv2.HoughLinesP(edges,1,np.pi/180,100,minLineLength,maxLineGap)
+for x1,y1,x2,y2 in lines[0]:
+    cv2.line(img,(x1,y1),(x2,y2),(0,255,0),2)
 
-# Make a plate with 4 holes in it at various points in a polar arrangement from
-# the center of the workplane.
-# 1.  Establishes a workplane that an object can be built on.
-# 1a. Uses the named plane orientation "front" to define the workplane, meaning
-#     that the positive Z direction is "up", and the negative Z direction
-#     is "down".
-# 2.  A 2D circle is drawn that will become though outer profile of the plate.
-r = cq.Workplane("front").circle(plate_radius)
-
-# 3. Push 4 points on the stack that will be used as the center points of the
-#    holes.
-r = r.pushPoints([(1.5, 0), (0, 1.5), (-1.5, 0), (0, -1.5)])
-
-# 4. This circle() call will operate on all four points, putting a circle at
-#    each one.
-r = r.rect(hole_pattern_radius,hole_pattern_radius)
-
-# 5.  All 2D geometry is extruded to the specified thickness of the plate.
-# 5a. The small hole circles are enclosed in the outer circle of the plate and
-#     so it is assumed that we want them to be cut out of the plate.  A
-#     separate cut operation is not needed.
-result = r.extrude(thickness)
-s = StringIO()
-exporters.exportShape(result,exporters.ExportTypes.STL,s,0.1)
-
-with open ('file.stl', 'w') as fd:
-  s.seek (0)
-  shutil.copyfileobj (s, fd)
+plt.imshow(edges)
+plt.show()
--- settings/settings.py
+++ settings/settings.py
@@ -2,6 +2,14 @@
 
 #######--ROOM RELATED SETTINGS--#######
 
-size_x = 10 #m
-size_y = 10 #m
+size_x = 14 #m
+size_y = 14 #m
 size_z = 3 #m
+
+mm_to_m = 1000 #conversion
+wall_color = 255 # white wall colors
+empty_color = 0 # black empty color
+
+img_size_x = 140
+img_size_y = 140
+
